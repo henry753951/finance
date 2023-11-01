001b43d9b46ad5c6aea8b348a7e575ba559abc3c
henry <henryboy753951@gmail.com>
2023-11-01 15:35:59 +0800

    time oust 60

--- crawl/goodinfo.py
+++ crawl/goodinfo.py
@@ -6,7 +6,7 @@ import time
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.150 Safari/537.36"
 }
-TIMEOUT = 10
+TIMEOUT = 60
 prevCallTime = time.time()
 
 def getHistoryEquityPreYear(stock_id: str) -> dict[str, int]:
